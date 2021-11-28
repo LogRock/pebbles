252153780c20cec32ebb7e9bcb155d855ea2f68b
Felipe Pereira <felipe.pereira@logrock.com>
2021-11-28 10:47:40 -0300

    style(project): :art: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate')
+
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-property-sort-order-smacss'
+  ],
+  plugins: ['stylelint-order'],
+  customSyntax: '@stylelint/postcss-css-in-js',
+  rules: {
+    'order/properties-order': [
+      sortOrderSmacss({ noEmptyLineBetween: true, emptyLineBefore: 'always' })
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        '**/*.{js,jsx,ts,tsx}'
+      ]
+    }
+  ]
+}
