22776574ac83e89726bf5618a15f1063c13bf218
Felipe Pereira <felipe.pereira@logrock.com>
2021-11-28 10:04:56 -0300

    test(project): :white_check_mark: add testing setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+  },
+  ignorePatterns: ['dist/', 'node_modules/']
+}
