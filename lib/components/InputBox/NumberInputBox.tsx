49a33db87acaafbded998798630ec9725eec09e7
Claudio dos Santos <claudio.santos@logrock.com>
2022-03-09 17:04:58 -0300

    chore(number-picker): styling button and inputbox

--- lib/components/InputBox/NumberInputBox.tsx
+++ lib/components/InputBox/NumberInputBox.tsx
@@ -4,16 +4,30 @@ import styled from "styled-components";
 import InputBox, { InputBoxProps } from "./InputBox";
 import Button from "../Button";
 
-const NumberInputBox: FC<InputBoxProps> = (props) => {
-  const NumberInputBoxWrapper = styled.div`
-    display: flex;
-  `;
+const NumberInputBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 4px 4px;
+`;
 
+const StyledInputBox = styled(InputBox)`
+  margin: 0px;
+
+  text-align: center;
+`;
+
+const StyledButton = styled(Button)`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const NumberInputBox: FC<InputBoxProps> = (props) => {
   return (
     <NumberInputBoxWrapper>
-      <Button>-</Button>
-      <InputBox />
-      <Button>+</Button>
+      <StyledButton>-</StyledButton>
+      <StyledInputBox />
+      <StyledButton>+</StyledButton>
     </NumberInputBoxWrapper>
   );
 };
