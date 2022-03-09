9fc13cade39dfddc0c157687567b0cd4f63c8b0b
Felipe Pereira <felipe.pereira@logrock.com>
2022-03-09 15:37:24 -0300

    feat(button): :sparkles: make Button accept all props from button tag

--- lib/components/Button/Button.types.ts
+++ lib/components/Button/Button.types.ts
@@ -1,10 +1,11 @@
 import React from "react";
 
-export type ButtonProps = {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "destructive";
   buttonStyle?: "primary" | "secondary" | "tertiary" | "outlined";
   buttonSize?: "sXmall" | "small" | "medium" | "large";
   iconOnly?: boolean;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
-};
+}
