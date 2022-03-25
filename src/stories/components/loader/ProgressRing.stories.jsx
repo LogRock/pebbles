403aab52e5db85b268b6320f71d17d2406803f1a
Luiz Daniel Lima <luizdml@gmail.com>
2022-03-25 15:17:37 -0300

    feat(added new loader ring component): loader ring substitutes old progress loader component
    BREAKING CHANGE: removed old ProgressLoader component

---
+++ src/stories/components/loader/ProgressRing.stories.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ProgressRing from "../../../../lib/components/Loader/ProgressRing";
+
+export default {
+  title: "Components/ProgressRing/Default",
+  component: ProgressRing,
+};
+
+const Template = (args) => <ProgressRing {...args} />;
+
+export const Default = Template.bind();
+Default.args = {
+  percentage: 95,
+  radius: 40,
+  stroke: 8,
+  showBaseRing: true,
+};
+
+export const SolidColorNoBaseRing = Template.bind({});
+SolidColorNoBaseRing.args = {
+  percentage: 75,
+  radius: 30,
+  stroke: 4,
+  children: "75%",
+};
+
+export const GradientColor = Template.bind({});
+GradientColor.args = {
+  percentage: 40,
+  radius: 40,
+  stroke: 6,
+  colorGradient: true,
+  showBaseRing: true,
+  children: "2 of 5",
+};
