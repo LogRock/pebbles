9309de9b68ad5f6088b1ac04d2de5bdcfdc39c59
Claudio dos Santos <claudio.santos@logrock.com>
2022-05-13 12:58:58 -0300

    fix: turn on datepicker for input date

--- lib/components/InputBox/InputBox.tsx
+++ lib/components/InputBox/InputBox.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from "react";
 import BaseInputBox, { BaseInputBoxProps } from "./BaseInputBox";
-// import DateInputBox from "./DateInputBox";
+import DateInputBox from "./DateInputBox";
 import NumberInputBox from "./NumberInputBox";
 
 const InputBox: FC<BaseInputBoxProps> = (props) => {
   if (props.type === "number") return <NumberInputBox {...props} />;
-  // if (props.type === "date") return <DateInputBox {...props} />;
+  if (props.type === "date") return <DateInputBox {...props} />;
   else return <BaseInputBox {...props} />;
 };
 
