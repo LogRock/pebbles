32219d5d5561fe502363908132de90fbe341fbbe
Felipe Pereira <felipe.pereira@logrock.com>
2022-05-24 20:26:38 -0300

    test(project): :white_check_mark: add test setup

---
+++ lib/components/Alert/Alert.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "@testing-library/jest-dom";
+// we should also import screen, userEvent, etc from this file.
+import { render } from "../../utils/test-utils";
+import Alert from ".";
+
+describe("Alert", () => {
+  it("renders properly", () => {
+    const { container } = render(<Alert>Foo</Alert>);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
