fe3c9ac269075b3010fe9c88445223cac14ecdcf
Claudio dos Santos <claudio.santos@logrock.com>
2022-05-31 17:08:14 -0300

    feat: pebbles phone input box

--- lib/components/InputBox/InputBox.tsx
+++ lib/components/InputBox/InputBox.tsx
@@ -2,10 +2,12 @@ import React, { FC } from "react";
 import BaseInputBox, { BaseInputBoxProps } from "./BaseInputBox";
 import DateInputBox from "./DateInputBox";
 import NumberInputBox from "./NumberInputBox";
+import PhoneInputBox from "./PhoneInputBox";
 
 const InputBox: FC<BaseInputBoxProps> = (props) => {
   if (props.type === "number") return <NumberInputBox {...props} />;
   if (props.type === "date") return <DateInputBox {...props} />;
+  if (props.type === "tel") return <PhoneInputBox {...props} />;
   else return <BaseInputBox {...props} />;
 };
 
