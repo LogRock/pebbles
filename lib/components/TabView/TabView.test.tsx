c1e85c79b13042d5e4ceaffced6f5404146684ed
Luiz Daniel Lima <luizdml@gmail.com>
2022-06-07 16:53:24 -0300

    feat: added TabView unit test

---
+++ lib/components/TabView/TabView.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "@testing-library/jest-dom";
+import { render, screen } from "../../utils/test-utils";
+
+import TabView from "./TabView";
+import TabLinks from "./TabLinks";
+import TabLink from "./TabLink";
+import TabContent from "./TabContent";
+
+describe("TabView", () => {
+  it("renders TabView properly", () => {
+    const { container } = render(
+      <TabView>
+        <TabLinks>
+          <TabLink isActive>Tab 1</TabLink>
+          <TabLink>Tab 2</TabLink>
+          <TabLink disabled>Tab 3</TabLink>
+        </TabLinks>
+        <TabContent>
+          <h4>Tab Content here</h4>
+        </TabContent>
+      </TabView>
+    );
+
+    expect(screen.getByText("Tab 1") as HTMLAnchorElement).toBeInTheDocument();
+    expect(screen.getByText("Tab Content here")).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
