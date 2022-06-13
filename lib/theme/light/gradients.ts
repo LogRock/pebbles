c69d6cabfbd2f26bda3c6097eeb301fb04cdaef8
Felipe Pereira <felipe.pereira@logrock.com>
2022-06-13 18:49:09 -0300

    feat(theme): :lipstick: add gradient colors breakdown

--- lib/theme/light/gradients.ts
+++ lib/theme/light/gradients.ts
@@ -1,33 +1,53 @@
 export const sunburst = {
   background: "linear-gradient(98.41deg, #FF5F6D 0%, #FFC371 100%)",
+  colors: ["#FF5F6D", "#FFC371"],
+  angle: 98.41,
 };
 export const darkWineSea = {
   background: "linear-gradient(98.41deg, #16BFFD 0%, #CB3066 100%)",
+  colors: ["#16BFFD", "#CB3066"],
+  angle: 98.41,
 };
 export const cherryCola = {
   background: "linear-gradient(98.41deg, #EECDA3 0%, #EF629F 100%)",
+  colors: ["#EECDA3", "#EF629F"],
+  angle: 98.41,
 };
 export const coldSky = {
   background: "linear-gradient(98.41deg, #4CA1AF 0%, #C4E0E5 100%)",
+  colors: ["#4CA1AF", "#C4E0E5"],
+  angle: 98.41,
 };
 export const oliveGarden = {
   background:
     "linear-gradient(98.41deg, #F1F2B5 0%, #135058 100%, #135058 100%)",
+  colors: ["#F1F2B5", "#135058", "#135058"],
+  angle: 98.41,
 };
 export const morningSakura = {
   background: "linear-gradient(98.41deg, #FF6E7F 0%, #BFE9FF 100%)",
+  colors: ["#FF6E7F", "#BFE9FF"],
+  angle: 98.41,
 };
 export const blueMoon = {
   background: "linear-gradient(98.41deg, #1D2B64 0%, #F8CDDA 100%)",
+  colors: ["#1D2B64", "#F8CDDA"],
+  angle: 98.41,
 };
 export const synthwave = {
   background: "linear-gradient(98.41deg, #FC00FF 0%, #00B6DE 100%)",
+  colors: ["#FC00FF", "#00B6DE"],
+  angle: 98.41,
 };
 export const teaLeaves = {
   background: "linear-gradient(98.41deg, #02AAB0 0%, #00CDAC 100%)",
+  colors: ["#02AAB0", "#00CDAC"],
+  angle: 98.41,
 };
 export const blueberryWine = {
   background: "linear-gradient(98.41deg, #4568DC 0%, #B06AB3 100%)",
+  colors: ["#4568DC", "#B06AB3"],
+  angle: 98.41,
 };
 
 // MeshGradients still not CSS definable
