778fb0205b0b98424a6130595915c28d1db90a54
Luiz Daniel Lima <luizdml@gmail.com>
2022-07-22 16:40:56 -0300

    feat: now ProgressRing accepts variant param, changes color accordingly

--- src/stories/components/loader/ProgressRing.stories.jsx
+++ src/stories/components/loader/ProgressRing.stories.jsx
@@ -16,6 +16,36 @@ Default.args = {
   showBaseRing: true,
 };
 
+export const Primary = Template.bind();
+Primary.args = {
+  percentage: 65,
+  radius: 40,
+  stroke: 8,
+  showBaseRing: true,
+  variant: "primary",
+  label: "65%",
+};
+
+export const Success = Template.bind();
+Success.args = {
+  percentage: 35,
+  radius: 40,
+  stroke: 8,
+  showBaseRing: true,
+  variant: "success",
+  label: "35%",
+};
+
+export const Error = Template.bind();
+Error.args = {
+  percentage: 15,
+  radius: 40,
+  stroke: 8,
+  showBaseRing: true,
+  variant: "error",
+  label: "15%",
+};
+
 export const SolidColorNoBaseRing = Template.bind({});
 SolidColorNoBaseRing.args = {
   percentage: 75,
@@ -26,7 +56,7 @@ SolidColorNoBaseRing.args = {
 
 export const GradientColor = Template.bind({});
 GradientColor.args = {
-  percentage: 40,
+  percentage: 80,
   radius: 40,
   stroke: 6,
   colorGradient: true,
