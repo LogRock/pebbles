9b8494a6272acda39cb64ab6dc2c2a9291dc7de0
Felipe Pereira <felipe.pereira@logrock.com>
2022-07-22 19:22:42 -0300

    fix(tab-view): :lipstick: fix tab alignment to the left

--- lib/components/TabView/TabLinks.tsx
+++ lib/components/TabView/TabLinks.tsx
@@ -4,7 +4,7 @@ const TabLinks = styled.div`
   display: flex;
   flex-flow: row nowrap;
   align-items: stretch;
-  justify-content: space-between;
+  justify-content: flex-start;
 `;
 
 export default TabLinks;
