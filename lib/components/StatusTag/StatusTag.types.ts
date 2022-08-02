06b0982f576d877d9214d3dc9bb5ac17a231b17d
Luiz Daniel Lima <luizdml@gmail.com>
2022-08-02 12:15:26 -0300

    fix: fixed StatusTag variant types

--- lib/components/StatusTag/StatusTag.types.ts
+++ lib/components/StatusTag/StatusTag.types.ts
@@ -3,7 +3,13 @@ import React from "react";
 export interface StatusTagProps extends React.HTMLAttributes<HTMLDivElement> {
   label?: string;
   size: "micro" | "small" | "medium" | "large";
-  variant?: "error" | "warning" | "success" | "neutral";
+  variant?:
+    | "error"
+    | "errorAlt"
+    | "warning"
+    | "warningAlt"
+    | "success"
+    | "neutral";
   icon?: React.ReactNode;
   collapsed?: boolean | false;
 }
