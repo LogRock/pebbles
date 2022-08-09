420a9c8f4a13834a49c281771d33615f59c2624d
Luiz Daniel Lima <luizdml@gmail.com>
2022-08-09 15:48:37 -0300

    test: added some unit tests to StatusTrend component

--- lib/components/Loader/ProgressRing.test.tsx
+++ lib/components/Loader/ProgressRing.test.tsx
@@ -3,6 +3,18 @@ import { render, screen } from "../../utils/test-utils";
 import ProgressRing from "./ProgressRing";
 
 describe("ProgressRing tests", () => {
+  it("matches the component snapshot", () => {
+    const { container } = render(
+      <ProgressRing
+        percentage={75}
+        radius={30}
+        stroke={4}
+        label="75%"
+      ></ProgressRing>
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
   it("shows the number percentage", () => {
     render(
       <ProgressRing
