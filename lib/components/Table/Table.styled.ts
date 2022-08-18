1648ce90f89f8a54099b8fab190133c3d0500a0a
Luiz Daniel Lima <luizdml@gmail.com>
2022-08-18 16:10:47 -0300

    feat: select menu opens up or downwards, according to dist to window bottom

--- lib/components/Table/Table.styled.ts
+++ lib/components/Table/Table.styled.ts
@@ -1,6 +1,5 @@
 import styled, { css } from "styled-components";
 import Select from "../Select";
-import { SelectItems } from "../Select/Select.styled";
 import {
   overlineXSmallCSS,
   paragraphMediumCSS,
@@ -140,9 +139,7 @@ export const TablePerPageLabel = styled.label`
   text-transform: uppercase;
 `;
 
-export const TablePerPageSelector = styled(Select)<
-  Pick<any, "autoCompleteItems" | "expandUp">
->`
+export const TablePerPageSelector = styled(Select)`
   max-width: 120px;
   margin-right: ${({ theme }) => theme.spacings.xbig};
 
@@ -157,11 +154,6 @@ export const TablePerPageSelector = styled(Select)<
     display: flex;
     flex-direction: column;
     justify-content: center;
-  }
-
-  ${SelectItems} {
-    top: ${({ autoCompleteItems, expandUp }) =>
-      expandUp ? autoCompleteItems.length * -36 : 48}px;
   }
 `;
 
