90dd7b03f6f74bf255d19e030114111a979150e6
Luiz Daniel Lima <luizdml@gmail.com>
2022-08-31 09:58:39 -0300

    feat: stepper - hides active and hover effects according to two new props

--- lib/components/Stepper/Stepper.types.ts
+++ lib/components/Stepper/Stepper.types.ts
@@ -2,5 +2,7 @@ export interface StepperProps {
   labels?: string[];
   finishedSteps?: number[];
   curStep?: number;
+  forceTwoDigitsIndicator?: boolean;
+  showHoverEffect?: boolean;
   onStepClick?: (stepNum: number) => void;
 }
