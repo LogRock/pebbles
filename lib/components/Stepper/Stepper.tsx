39f2a1820ac052c71087ba70cbc0d17f30be44fd
Luiz Daniel Lima <luizdml@gmail.com>
2022-09-01 09:49:33 -0300

    feat: stepper items layout css all ok now

--- lib/components/Stepper/Stepper.tsx
+++ lib/components/Stepper/Stepper.tsx
@@ -1,77 +1,16 @@
-import React from "react";
+import React, { FC } from "react";
 
-import {
-  Step,
-  StepCheckMark,
-  StepIndex,
-  StepLabel,
-  StepLine,
-  Steps,
-} from "./Stepper.styled";
+import { Steps } from "./Stepper.styled";
 import { StepperProps } from "./Stepper.types";
 
-const Stepper = (props: StepperProps) => {
-  const {
-    labels,
-    finishedSteps,
-    curStep,
-    onStepClick,
-    forceTwoDigitsIndicator = false,
-    showHoverEffect = true,
-  } = props;
-
-  const adjustStepDisplay = (stepNum: number) => {
-    if (forceTwoDigitsIndicator && stepNum < 10) {
-      return stepNum.toString().padStart(2, "0");
-    }
-    return stepNum.toString();
-  };
-
-  const stepIsFinished = (stepNum: number) => {
-    if (finishedSteps && finishedSteps.length !== 0) {
-      return finishedSteps?.indexOf(stepNum) !== -1;
-    }
-    return false;
-  };
-
-  let itens;
-  if (labels) {
-    itens = labels.map((label, index) => {
-      const stepNum = index + 1;
-      const last = stepNum === labels.length;
-      const current = stepNum === curStep;
-      const finished = stepIsFinished(stepNum);
-
-      return (
-        <Step
-          key={`${index}_${label}`}
-          onClick={() => {
-            onStepClick && onStepClick(stepNum);
-          }}
-          showHoverEffect={showHoverEffect}
-        >
-          <StepIndex
-            isCurStep={current}
-            isLastItem={last}
-            isFinished={finished}
-          >
-            <p>{adjustStepDisplay(stepNum)}</p>
-            <StepLine
-              isCurStep={current}
-              isLastItem={last}
-              isFinished={finished}
-            />
-            <StepCheckMark isCurStep={current} isFinished={finished} />
-          </StepIndex>
-
-          {/* TO-DO: must accept React.Node, have a more versatile step content */}
-          <StepLabel isCurStep={current}>{label}</StepLabel>
-        </Step>
-      );
-    });
-  }
-
-  return <Steps>{itens}</Steps>;
+const Stepper: FC<StepperProps> = ({
+  onStepClick,
+  forceTwoDigitsIndicator = false,
+  showHoverEffect = true,
+  children,
+  ...props
+}) => {
+  return <Steps>{children}</Steps>;
 };
 
 export default Stepper;
--- src/stories/components/stepper/Stepper.stories.js
+++ src/stories/components/stepper/Stepper.stories.js
@@ -1,23 +1,83 @@
 import React from "react";
+import styled from "styled-components";
+
 import Stepper from "../../../../lib/components/Stepper";
+import StepperStep from "../../../../lib/components/Stepper/StepperStep";
 
 export default {
   title: "Components/Stepper",
   component: Stepper,
 };
 
-const Template = (args) => <Stepper {...args}>{args.label}</Stepper>;
+const NoMarginText = styled.h4`
+  margin: 0;
+`;
+
+const Template = (args) => <Stepper {...args}></Stepper>;
 
 export const Default = Template.bind({});
 Default.args = {
-  labels: ["Personal", "Company", "ELD", "Documents", "Finish"],
-  finishedSteps: [1, 2, 3],
-  curStep: 4,
+  forceTwoDigitsIndicator: true,
+  showHoverEffect: true,
+  children: (
+    <>
+      <StepperStep stepIndex={1} completed>
+        <NoMarginText>Personal</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={2} completed>
+        <NoMarginText>Company</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={3} current>
+        <NoMarginText>ELD</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={4} last>
+        <NoMarginText>Finish</NoMarginText>
+      </StepperStep>
+    </>
+  ),
 };
 
 export const InformativeOnly = Template.bind({});
 InformativeOnly.args = {
-  labels: ["Personal", "Company", "ELD", "Documents", "Finish"],
-  forceTwoDigitsIndicator: false,
-  showHoverEffect: false,
+  forceTwoDigitsIndicator: true,
+  showHoverEffect: true,
+  children: (
+    <>
+      <StepperStep stepIndex={1}>
+        <NoMarginText>Personal</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={2}>
+        <NoMarginText>Company</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={3}>
+        <NoMarginText>ELD</NoMarginText>
+      </StepperStep>
+      <StepperStep stepIndex={4}>
+        <NoMarginText>Documents</NoMarginText>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
+          consequatur ab quas aperiam, eos modi voluptas sit autem tenetur ipsa
+          ex reprehenderit illum voluptates iste, dolorum explicabo. Quaerat,
+          laborum ab? Quos autem suscipit libero commodi reiciendis quam minima,
+          molestias rerum nisi officiis itaque. Debitis expedita qui, velit
+          dolorum quidem tempora aperiam et distinctio perferendis reiciendis
+          hic! Reiciendis nostrum ullam incidunt vel unde at quod assumenda ea
+          modi eos vero labore explicabo debitis iusto ex, temporibus illo
+          repellat sequi. Facere, atque!
+        </p>
+      </StepperStep>
+      <StepperStep stepIndex={5} last>
+        <NoMarginText>Last Step</NoMarginText>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            voluptatem delectus at dolorem, modi harum, omnis ea atque saepe
+            architecto aperiam! Magnam obcaecati ipsa eveniet at deleniti labore
+            non minus suscipit iure placeat cupiditate quas, vero facilis
+            commodi accusamus nemo.
+          </p>
+        </div>
+      </StepperStep>
+    </>
+  ),
 };
