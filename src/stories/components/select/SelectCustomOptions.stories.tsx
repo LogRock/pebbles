326c968b215a7604bc964973128030244e66ebfb
Felipe Pereira <felipe.pereira@logrock.com>
2022-09-14 13:23:51 -0300

    feat(select): :sparkles: port the checkbox item to the new select model

---
+++ src/stories/components/select/SelectCustomOptions.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Select from "../../../../lib/components/Select2";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CheckBoxOption from "../../../../lib/components/Select2/items/CheckBoxItem";
+
+const autoCompleteItems = [
+  {
+    value: "1",
+    label: "First Item",
+  },
+  {
+    value: "2",
+    label: "Second Item",
+  },
+  {
+    value: "3",
+    label: "Third Item",
+  },
+  {
+    value: "4",
+    label: "You guessed it",
+  },
+];
+
+export default {
+  title: "Components/Select/CustomOptions",
+  component: Select,
+  argTypes: { onChange: { action: "changed" } },
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => (
+  <Select {...args} options={autoCompleteItems} />
+);
+
+export const WithCheckBoxes = Template.bind({});
+WithCheckBoxes.args = {
+  isClearable: true,
+  isMulti: true,
+  isSearchable: true,
+  hideSelectedOptions: false,
+  label: "This shows checkboxes instead of plain text",
+  helper: "Using the isMulti prop is probably a good idea in this case",
+  components: {
+    Option: CheckBoxOption,
+  },
+};
+
+export const WithRadioButtons = Template.bind({});
