b85f1088ec3c7637f3e76ed9d8752fa32bf15b7c
Felipe Pereira <felipe.pereira@logrock.com>
2022-11-23 16:31:56 -0300

    feat(select): :lipstick: update styling for dropdown icons

---
+++ lib/components/Select2/Select.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DropdownIndicatorWrapper = styled.div`
+  padding: ${({ theme }) => theme.icons.spacing};
+
+  color: ${({ theme }) => theme.icons.input.dropdown.color};
+`;
