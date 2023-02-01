16ff7cb1e2fcab5d82157779aab877842d7bd520
Felipe Pereira <felipe.pereira@logrock.com>
2023-02-01 16:52:48 -0300

    docs(text): :memo: add more stories to text component

--- src/stories/foundations/typography/Text.stories.tsx
+++ src/stories/foundations/typography/Text.stories.tsx
@@ -76,10 +76,88 @@ const Template: Story<PropsWithChildren<TextProps>> = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  children: "Title",
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
   color: "neutral",
   shade: "900",
   type: "paragraphMedium",
+  weight: "normal",
+  as: "p",
+};
+
+export const OverlineLarge = Template.bind({});
+OverlineLarge.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "overlineLarge",
+  weight: "normal",
+  as: "p",
+};
+
+export const OverlineMedium = Template.bind({});
+OverlineMedium.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "overlineMedium",
+  weight: "normal",
+  as: "p",
+};
+
+export const OverlineXSmall = Template.bind({});
+OverlineXSmall.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "overlineXSmall",
+  weight: "normal",
+  as: "p",
+};
+
+export const ParagraphLarge = Template.bind({});
+ParagraphLarge.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "paragraphLarge",
+  weight: "normal",
+  as: "p",
+};
+
+export const ParagraphMedium = Template.bind({});
+ParagraphMedium.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "paragraphMedium",
+  weight: "normal",
+  as: "p",
+};
+
+export const ParagraphSmall = Template.bind({});
+ParagraphSmall.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "paragraphSmall",
+  weight: "normal",
+  as: "p",
+};
+
+export const ParagraphXSmall = Template.bind({});
+ParagraphXSmall.args = {
+  children:
+    "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
+  color: "neutral",
+  shade: "900",
+  type: "paragraphXSmall",
   weight: "normal",
   as: "p",
 };
