f171aa7c4a102bed5f27968467aabe6e7cc0babb
Felipe Pereira <felipe.pereira@logrock.com>
2023-08-30 16:32:11 -0300

    feat(date-input): :lipstick: customize styles to match

--- lib/components/DateInput/DateInput.tsx
+++ lib/components/DateInput/DateInput.tsx
@@ -3,6 +3,34 @@ import { DateInputProps } from "./DateInput.types";
 import { DatePicker } from "@mui/x-date-pickers";
 import uniqueId from "lodash.uniqueid";
 import { CustomDateField } from "./CustomField";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  .MuiPickersDay-root:hover:not(.Mui-disabled), .MuiPickersDay-today {
+    background-color: ${({ theme }) => theme.colors.primary[50]} !important;
+
+    color: ${({ theme }) => theme.colors.neutral[900]} !important;
+  }
+  .Mui-selected {
+    background-color: ${({ theme }) => theme.colors.primary[200]} !important;
+
+    color: ${({ theme }) => theme.colors.neutral[900]} !important;
+  }
+  .MuiPaper-elevation {
+    box-shadow: ${({ theme }) => theme.shadows.medium} !important;
+  }
+  .MuiPaper-rounded {
+    border-radius: ${({ theme }) => theme.spacings.md} !important;
+  }
+  .MuiButton-textPrimary {
+    color: ${({ theme }) => theme.colors.neutral[900]} !important;
+  }
+
+  .MuiDialog-root * {
+    font-family: ${({ theme }) =>
+      theme.typography.paragraphMedium.fontFamily} !important;
+  }
+`;
 
 export default function DateInput({
   helper,
@@ -14,21 +42,29 @@ export default function DateInput({
   const inputID = id || uniqueId("pebbles__input__");
 
   return (
-    <DatePicker
-      {...props}
-      slotProps={{
-        field: {
-          id: inputID,
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          helperText: helper,
-          helperIcon: helperIcon,
-          error: destructive,
-        },
-      }}
-      slots={{
-        field: CustomDateField,
-      }}
-    />
+    <>
+      <GlobalStyle />
+      <DatePicker
+        {...props}
+        sx={{
+          ".MuiPickersDay-root.Mui-selected": {
+            backgroundColor: "red",
+          },
+        }}
+        slotProps={{
+          field: {
+            id: inputID,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            helperText: helper,
+            helperIcon: helperIcon,
+            error: destructive,
+          },
+        }}
+        slots={{
+          field: CustomDateField,
+        }}
+      />
+    </>
   );
 }
