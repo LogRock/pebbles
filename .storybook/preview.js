4420c266349db434523101397f5aecaa5bdeb0a7
Felipe Pereira <felipe.pereira@logrock.com>
2023-09-03 15:23:24 -0300

    fix(storybook): :bug: fix docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,9 @@ import { withThemes } from "@react-theming/storybook-addon";
 import { light, dark } from "../lib/theme";
 import { addDecorator } from "@storybook/react";
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
 import "@fontsource/archivo";
 import "@fontsource/archivo/variable-full.css";
 
@@ -24,8 +27,10 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <IconContext.Provider value={{ style: { fontSize: 20 } }}>
-      {<Story />}
-    </IconContext.Provider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <IconContext.Provider value={{ style: { fontSize: 20 } }}>
+        {<Story />}
+      </IconContext.Provider>
+    </LocalizationProvider>
   ),
 ];
